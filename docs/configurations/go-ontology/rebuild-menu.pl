215c2a6f23b97a5e0bddc26e60df141caa0dac94
Chris Mungall <cjm@berkeleybop.org>
2017-08-14 15:44:16 -0700

    regen

--- docs/configurations/go-ontology/rebuild-menu.pl
+++ docs/configurations/go-ontology/rebuild-menu.pl
@@ -1,9 +1,14 @@
 #!/usr/bin/perl
-my @files = split(/\n/,`ls $ENV{HOME}/repos/go-ontology/src/design_patterns/*yaml`);
+my @files = split(/\n/,`ls $ENV{HOME}/repos/go-ontology/src/design_patterns/*{yaml,tsv}`);
 print "defaultPatterns:\n";
-foreach (@files) {
+foreach (grep {/\.yaml$/} @files) {
     s@.*/@@;
     print "  - url: https://raw.githubusercontent.com/geneontology/go-ontology/master/src/design_patterns/$_\n";
     print "    title: $_\n";
 }
 print "defaultXSVs:\n";
+foreach (grep {/\.tsv$/} @files) {
+    s@.*/@@;
+    print "  - url: https://raw.githubusercontent.com/geneontology/go-ontology/master/src/design_patterns/$_\n";
+    print "    title: $_\n";
+}
