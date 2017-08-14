f4bf8dafaf28d656d7afd73b4f00c09e0a1c8845
Chris Mungall <cjm@berkeleybop.org>
2017-08-14 14:54:35 -0700

    new

---
+++ docs/configurations/go-ontology/rebuild-menu.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+my @files = split(/\n/,`ls $ENV{HOME}/repos/go-ontology/src/design_patterns/*yaml`);
+print "defaultPatterns:\n";
+foreach (@files) {
+    s@.*/@@;
+    print "  - url: https://raw.githubusercontent.com/geneontology/go-ontology/master/src/design_patterns/validated_patterns/$_\n";
+    print "    title: $_\n";
+}
+print "defaultXSVs:\n";
